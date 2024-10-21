c855e6d2f727fd242e5224455bf8b50b38568b2c
burigooth <mateusburigocontato@gmail.com>
2024-10-20 23:14:14 -0300

    ✨feat: adicionar validação nos campos

--- src/main/java/com/med/api/endereco/DadosEndereco.java
+++ src/main/java/com/med/api/endereco/DadosEndereco.java
@@ -1,4 +1,24 @@
 package com.med.api.endereco;
 
-public record DadosEndereco(String logradouro, String bairro, String cep, String complemento, String cidade, String uf, String numero) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosEndereco(
+        @NotBlank
+        String logradouro,
+
+        @NotBlank
+        String bairro,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{8}")
+        String cep,
+
+        String complemento,
+        String numero,
+        @NotBlank
+        String cidade,
+
+        @NotBlank
+        String uf) {
 }
