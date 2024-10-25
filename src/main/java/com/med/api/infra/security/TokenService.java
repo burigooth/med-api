bc0976b47e10e2242ed772f14479408ee0bf4075
burigooth <mateusburigocontato@gmail.com>
2024-10-25 01:25:50 -0300

    ✨ feat: recuperar token para fazer o controle de acesso

--- src/main/java/com/med/api/infra/security/TokenService.java
+++ src/main/java/com/med/api/infra/security/TokenService.java
@@ -3,6 +3,8 @@ package com.med.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.med.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -33,6 +35,20 @@ public class TokenService {
         }
     }
 
+    // Método para verificar se o token é válido
+    public String getSubject(String tokenJWT){
+        DecodedJWT decodedJWT;
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API da Med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token inválido ou expirado", exception);
+        }
+    }
     private Instant dataExpiracao() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
